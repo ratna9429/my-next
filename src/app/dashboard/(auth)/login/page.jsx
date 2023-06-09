23519948efe2d67c3a7488e8d9da52d5a76ad296
Ratnadeep <ratnadeep@zeliot.in>
2023-06-09 12:08:54 +0530

    Route and Css page set-up

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "./page.module.css";
 
-const About = () => {
-  return <div>About</div>;
+const Login = () => {
+  return <div>Login</div>;
 };
 
-export default About;
+export default Login;
